a071c117d839d516986311f9457d4d1b0f4730d0
NAVEEN <46046229+i-am-naveen-m-j@users.noreply.github.com>
2024-01-18 13:52:28 +0530

    Add files via upload

---
+++ DevOps/Terraform Zero to Hero/ansible_1/main.tf
@@ -0,0 +1,145 @@
+# install Ansible only on the "ansible-master" instance and not on the "ansible-slave" instance,
+
+# Define the AWS provider configuration.
+provider "aws" {
+  region = "ap-south-1"  # Replace with your desired AWS region.
+}
+
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+resource "aws_key_pair" "example" {
+  key_name   = "terraform-demo-naveen"  # Replace with your desired key name
+  public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key file
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.cidr
+}
+
+resource "aws_subnet" "sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_security_group" "webSg" {
+  name   = "web"
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-sg"
+  }
+}
+
+resource "aws_instance" "ansible-master" {
+  ami                    = "ami-03f4878755434977f"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub1.id
+
+  tags = {
+    Name = "ansible-master"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
+    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      #"echo 'Hello from ansible-master instance'",
+      #"sudo apt update -y",  # Update package lists (for ubuntu)
+      #"sudo apt install -y ansible",  # Install Ansible
+      # Add additional provisioning steps as needed for ansible-master
+
+
+      "echo 'Hello from ansible-master instance'",
+      "sudo apt update -y",
+      "sudo apt install -y python",  # Install Python
+      "sudo apt install -y software-properties-common",  # Install the software-properties-common package
+      "sudo apt-add-repository --yes --update ppa:ansible/ansible",  # Add Ansible repository
+      "sudo apt update -y",  # Update package lists after adding the repository
+      "sudo apt install -y ansible",  # Install Ansible
+    ]
+  }
+}
+
+/*
+resource "aws_instance" "ansible-slave" {
+  ami                    = "ami-03f4878755434977f"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub1.id
+
+  tags = {
+    Name = "ansible-slave"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
+    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
+    host        = self.public_ip
+  }
+
+  # No provisioning steps for Ansible on the ansible-slave instance
+}
+
+*/
