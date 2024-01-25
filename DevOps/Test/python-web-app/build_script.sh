6bd70fb3fa315ddf4eb27ecc7a9d56bd0d9828a4
NAVEEN <46046229+i-am-naveen-m-j@users.noreply.github.com>
2024-01-25 18:32:13 +0530

    Update build_script.sh

--- DevOps/Test/python-web-app/build_script.sh
+++ DevOps/Test/python-web-app/build_script.sh
@@ -1,23 +1,27 @@
-#!/bin/bash
+
  
 # Navigate to your Python project directory
 # cd /path/to/your/python/project
  
 # Install dependencies (if needed)
+
+
+
+
+#!/bin/bash
+echo "started shell script execution from here"
 ls -la
 pip install -r requirements.txt
-
 cd devops
+python3 manage.py runserver 0.0.0.0:8000
 
-pwd
-ls -la
 
-nohup python3 manage.py runserver 0.0.0.0:8000 > server_log.txt 2>&1 &
-disown
+
+
+
+# nohup python3 manage.py runserver 0.0.0.0:8000 > server_log.txt 2>&1 & disown
 
 # // nohup python3 manage.py runserver 0.0.0.0:8000
 
-
- 
 # Run your Python script to build the project
 #python YOUR_PYTHON_SCRIPT.py
